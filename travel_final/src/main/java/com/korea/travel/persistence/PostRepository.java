8a652749af7c63e406c710720dd25f251ec9e8dc
jjunik <tjwnsdlr4545@gmail.com>
2024-12-19 17:45:48 +0900

    ...

--- travel_final/src/main/java/com/korea/travel/persistence/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.korea.travel.persistence;
-
-import com.korea.travel.model.PostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<PostEntity, Long> {
-}
